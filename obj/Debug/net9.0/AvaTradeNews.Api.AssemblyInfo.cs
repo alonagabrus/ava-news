b2229dd1398ec3b6a3d0414340058e549a307152
Alona <alonagabrus@gmail.com>
2025-08-30 18:25:52 +0300

    Add .gitignore and clean up repo

--- obj/Debug/net9.0/AvaTradeNews.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/AvaTradeNews.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AvaTradeNews.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c170daecf8ee98ba4c37327ddcf1bcecdfad194")]
 [assembly: System.Reflection.AssemblyProductAttribute("AvaTradeNews.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AvaTradeNews.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
